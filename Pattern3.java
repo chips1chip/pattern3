6cf6312f1fbb9a52a7614c53319631e08858809c
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 10:20:59 +0530

    Update Pattern3.java

--- Pattern3.java
+++ Pattern3.java
@@ -1,9 +1,17 @@
-public class Pattern3 {
+public class Pattern1 {
     public static void main(String[] args) {
-        System.out.println("*   *");
-        System.out.println(" * * ");
-        System.out.println("  *  ");
-        System.out.println(" * * ");
-        System.out.println("*   *");
+    
+        // Declare variables i and j 
+        int i,j;
+        // Outer loop controls the number of rows (5 rows in total)
+        for(i = 0; i <= 4; i++) {
+            // Inner loop controls the number of 'x' printed in each row
+            for(j = 0; j <= i; j++) {
+                // Print 'x' without moving to the next line
+                System.out.print("x");
+            }
+            // Move to the next line after printing all 'x' in a row
+            System.out.println("");
+        }
     }
 }
